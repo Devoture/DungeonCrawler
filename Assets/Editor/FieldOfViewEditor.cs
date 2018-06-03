26316969ba9cd1831c41ce3b8c93f565724dd5b5
Josh Brackett <joshua.brackett@hotmail.ca>
2018-06-03 17:38:13 -0400

    enemies now of LoS and wont shoot unless they can see the player, if they cant see the player they will keep chasing until they do. also added an editor script that will draw a GUI of the LoS in the scene

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (TestEnemy))]
+public class FieldOfViewEditor : Editor {
+
+	void OnSceneGUI() {
+		TestEnemy test = (TestEnemy)target;
+		Handles.color = Color.white;
+		Handles.DrawWireArc(test.transform.position, Vector3.up, Vector3.forward, 360, test.viewRadius);
+		Vector3 viewAngleA = test.DirFromAngle(-test.viewAngle / 2, false);
+		Vector3 viewAngleB = test.DirFromAngle(test.viewAngle / 2, false);
+
+		Handles.DrawLine(test.transform.position, test.transform.position + viewAngleA * test.viewRadius);
+		Handles.DrawLine(test.transform.position, test.transform.position + viewAngleB * test.viewRadius);
+
+		Handles.color = Color.red;
+		if(test.canSeePlayer) {
+			Handles.DrawLine(test.transform.position, test.player.position);
+		}
+	}
+}
--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -5,44 +5,56 @@ using UnityEngine.AI;
 
 public class TestEnemy : MonoBehaviour {
 
-	public Transform target;
+	public Transform player;
 	public Transform bulletSpawn;
 	public GameObject bulletPrefab;
 	public float attackRange = 10.0f;
 	public float rateOfFire = .5f;
 	public float timeBetweenShots = .25f;
 	public float bulletSpeed = 5.0f;
+	public float viewRadius;
+	[Range(0, 360)]
+	public float viewAngle;
+	public LayerMask targetMask;
+	public LayerMask obstacleMask;
+	public List<Transform> visibleTargets = new List<Transform>();
+	public bool canSeePlayer;
 
 	private NavMeshAgent agent;
 	private float distance;
-	public int numOfShots = 0;
-	public bool canShoot = true;
-	public bool canBurst = true;
+	private int numOfShots = 0;
+	private bool canShoot = true;
+	private bool canBurst = true;
+	private bool startedBurst;
 
 	void Start() {
 		agent = this.transform.GetComponent<NavMeshAgent>();
 	}
 
 	void Update() {
-		if(target != null) {
-			distance = Vector3.Distance(transform.position, target.position);
-			if(distance > attackRange) {
+		if(player != null) {
+			FindVisibleTargets();
+			distance = Vector3.Distance(transform.position, player.position);
+			if(distance > attackRange || !canSeePlayer) {
 				agent.isStopped = false;
-				agent.SetDestination(target.position);
-			} else {
+				agent.SetDestination(player.position);
+			} 
+			if(distance < attackRange && canSeePlayer || startedBurst) {
 				agent.isStopped = true;
 				if(canBurst && canShoot) {
+					startedBurst = true;
 					Attack();
 					canShoot = false;
 					StartCoroutine(BurstTimer(timeBetweenShots));
 				}
 				if(numOfShots == 3) {
 					canBurst = false;
+					startedBurst = false;
 					numOfShots = 0;
 					StartCoroutine(RateOfFire(rateOfFire));
 				}
 			}
-			transform.LookAt(target);
+			transform.LookAt(player);
 		}
 	}
 
@@ -51,6 +63,29 @@ public class TestEnemy : MonoBehaviour {
 		GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
 		bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
 		Destroy(bullet.gameObject, 2.0f);
+	}
+
+	void FindVisibleTargets() {
+		Collider[] TargetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+		for(int i = 0; i < TargetsInViewRadius.Length; i++) {
+			Transform target = TargetsInViewRadius[i].transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if(Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2) {
+				float dstToTarget = Vector3.Distance(transform.position, target.position);
+				if(!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+					canSeePlayer = true;
+				} else {
+					canSeePlayer = false;
+				}
+			}
+		}
+	}
+
+	public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+		if(!angleIsGlobal) {
+			angleInDegrees += transform.eulerAngles.y;
+		}
+		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
 	}
 
 	private IEnumerator BurstTimer(float waitTime) {
