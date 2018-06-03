af04a20d6b716401c26d9f1948b1d472741dc66c
Josh Brackett <joshua.brackett@hotmail.ca>
2018-06-03 01:44:27 -0400

    enemy can kill you and you can kill the enemy

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour {
+
+	public float damage = 20.0f;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			other.GetComponent<Health>().TakeDamage(damage);
+			Destroy(this.gameObject);
+		}
+	}
+}
