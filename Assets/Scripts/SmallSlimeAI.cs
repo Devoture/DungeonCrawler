1e6771afb39286cf78da60902936e9f4c2d9a884
joshua brackett <joshua.brackett@hotmail.ca>
2018-06-04 16:25:55 -0400

    made the small slime and hooked it up so when the big slime dies it spawns the 3 little slimes

---
+++ Assets/Scripts/SmallSlimeAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SmallSlimeAI : MonoBehaviour {
+
+	public GameObject player;
+	public Transform bulletSpawn;
+	public GameObject bulletPrefab;
+	public float rateOfFire = .5f;
+	public float bulletSpeed = 5.0f;
+	public RoomEntered roomEntered;
+
+	private NavMeshAgent agent;
+	private FieldOfView fov;
+	private float distance;
+	private bool canShoot = true;
+
+	void Start() {
+		agent = this.transform.GetComponent<NavMeshAgent>();
+		fov = GetComponent<FieldOfView>();
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update() {
+		if(player != null ) { // && roomEntered.playerIsInRoom
+			fov.FindVisibleTargets();
+			distance = Vector3.Distance(transform.position, player.transform.position);
+			if(distance > fov.attackRange || !fov.canSeePlayer) {
+				agent.isStopped = false;
+				agent.SetDestination(player.transform.position);
+			} 
+			if(distance < fov.attackRange && fov.canSeePlayer) {
+				agent.isStopped = true;
+				if(canShoot) {
+					Attack();
+					canShoot = false;
+					StartCoroutine(RateOfFire(rateOfFire));
+				}
+			}
+			transform.LookAt(player.transform);
+		}
+	}
+
+	void Attack() {
+		GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+		bullet.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+		Destroy(bullet.gameObject, 2.0f);
+	}
+
+	private IEnumerator RateOfFire(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		canShoot = true;
+    }
+}
