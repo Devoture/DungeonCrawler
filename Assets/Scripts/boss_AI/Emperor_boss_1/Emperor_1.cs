a7a00e38e5519d55c3675befc4c868990ecb3bdc
Tom Sauer <tom@exluto.com>
2018-06-07 21:58:28 -0400

    working on states

--- Assets/Scripts/boss_AI/Emperor_boss_1/Emperor_1.cs
+++ Assets/Scripts/boss_AI/Emperor_boss_1/Emperor_1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class boss_1 : MonoBehaviour {
+public class Emperor_1 : MonoBehaviour {
 
 	private StateMachine _stateMachine = new StateMachine();
 }
---
+++ Assets/Scripts/boss_AI/Emperor_boss_1/State1_1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// first State of Emperor Boss
+public class State1_1 : IState {
+
+	public void Enter() {
+
+	}
+
+	public void Execute() {
+
+	}
+
+	public void Exit() {
+
+	}
+}
